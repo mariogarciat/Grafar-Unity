ca8982f834e6767b270844b539b899bcb3e60f48
mariogarciat <andrex044@gmail.com>
2019-04-29 23:57:57 -0500

    files added

---
+++ Assets/Scripts/VRCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCamera : MonoBehaviour
+{
+    public VuforiaMonoBehaviour vuforiaBehavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vuforiaBehavior.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void activarCamara()
+    {
+        vuforiaBehavior.enabled = true;
+    }
+}
